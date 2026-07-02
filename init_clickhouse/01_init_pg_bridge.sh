2ff2c939e0ad34938aa1a320339d099f98f299d6
Andrei Pakkanen <a.pakkanen@me.com>
2026-07-02 10:34:24 +0500

    chore: add ClickHouse-Postgres bridge script with executable permissions

--- init_clickhouse/01_init_pg_bridge.sh
+++ init_clickhouse/01_init_pg_bridge.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-clickhouse-client --query="CREATE DATABASE IF NOT EXISTS postgres_dwh ENGINE = PostgreSQL('postgres-db:5432', 'restaurant', '${POSTGRES_USER}', '${POSTGRES_PASSWORD}');"
+set -e
+sleep 15
+clickhouse-client --query="CREATE DATABASE IF NOT EXISTS postgres_dwh ENGINE = PostgreSQL('postgres-db:5432', 'restaurant', '${POSTGRES_USER}', '${POSTGRES_PASSWORD}');" 
