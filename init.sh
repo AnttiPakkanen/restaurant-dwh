c4a5887247d367ce971181950cd8fd14cf2a873a
Andrei Pakkanen <a.pakkanen@me.com>
2026-07-02 19:27:29 +0500

    fix: update init script to handle root user on remote servers

--- init.sh
+++ init.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 mkdir -p ./dags ./logs ./plugins
 cp .env.example .env
-echo -e "\nAIRFLOW_UID=$(id -u)" >> .env
-echo "Папки созданы, права (UID $(id -u)) настроены в .env!"
+USER_ID=$(id -u)
+
+if [ "$USER_ID" -eq 0 ]; then
+    echo "Запуск от пользователя root (UID 0)."
+    echo "Airflow не может работать от root. Принудительно используем UID 50000 и меняем владельца папок."
+    chown -R 50000:0 ./dags ./logs ./plugins
+    echo -e "\nAIRFLOW_UID=50000" >> .env
+else
+    echo "Настройки для локального пользователя (UID $USER_ID)."
+    echo -e "\nAIRFLOW_UID=$USER_ID" >> .env
+fi
+
+echo "Папки созданы, права настроены в .env!"
